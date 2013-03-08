6508deb3bee8d64db0d29a59da9935f8441bad00
Exikle <dixondcunha@hotmail.com>
2013-03-08 04:41:19 -0600

    Initial Commit

---
+++ Organizer/src/com/xid_studios/java/utils/organizer/OrganizerGUI.java
@@ -0,0 +1,9 @@
+package com.xid_studios.java.utils.organizer;
+
+public class OrganizerGUI {
+	public static void main(String[] args) throws Exception {
+		new OrganizerGUI();
+	}
+	public OrganizerGUI(){
+	}
+}
