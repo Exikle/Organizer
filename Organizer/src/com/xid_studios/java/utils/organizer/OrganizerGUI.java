6be1cf31b088332ddcd6126e1a74d76e2c43a42f
Exikle <xidstudios@hotmail.com>
2013-08-16 04:10:31 -0400

    Signed-off-by: Exikle <xidstudios@hotmail.com>

--- Organizer/src/com/xid_studios/java/utils/organizer/OrganizerGUI.java
+++
@@ -1,9 +0,0 @@
-package com.xid_studios.java.utils.organizer;
-
-public class OrganizerGUI {
-	public static void main(String[] args) throws Exception {
-		new OrganizerGUI();
-	}
-	public OrganizerGUI(){
-	}
-}
---
+++ Organizer/src/com/xidstudios/java/utils/organizer/OrganizerGUI.java
@@ -0,0 +1,12 @@
+package com.xidstudios.java.utils.organizer;
+
+import javax.swing.JFrame;
+
+public class OrganizerGUI extends JFrame {
+
+	public OrganizerGUI() {
+	}
+
+	public static void main(String[] args) throws Exception {
+	}
+}
